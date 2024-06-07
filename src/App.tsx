d7caaf83da12124aabe623663814ca1e6b2d992f
Abiel Rodriguez <abiel.rodriguez@volenday.com>
2024-06-07 16:22:45 +0100

    c

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      TEST asd asdasd  asdasd
+      TEST asd asdasd  asdasd ads
     </>
   )
 }
